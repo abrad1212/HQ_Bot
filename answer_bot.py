f4e809d663e8eb88c171c1b4ddd4ff092f8b0dd0
abrad1212 <abrad12122@gmail.com>
2018-05-30 15:04:44 -0500

    Formatting fixes and added PEP8 tests

--- answer_bot.py
+++ answer_bot.py
@@ -2,34 +2,39 @@
 HQ Bot forked from https://github.com/sushant10/HQ_Bot
 
 Features:
-    Grab screenshots from phone screen (Reflector 3 or QuickTime Player required)
-    Multiprocess Google Search System
+    Grab screenshots from phone screen
+    Multithreaded Google Search System
+
+    Reflector 3 or QuickTime Player required
 """
 
 # answering bot for trivia HQ and Cash Show
 import os
 import sys
+import platform
 import time
 import json
-import argparse
-import platform
 from multiprocessing import Pool
 from functools import partial
+import urllib.request as urllib2
 
 import cv2
 from PIL import Image
 from PIL import ImageGrab
-import urllib.request as urllib2
 from bs4 import BeautifulSoup
 import numpy as np
 
 from google import google
 from halo import Halo
 import pytesseract
-pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe'
 
-# for terminal colors
-class bcolors:
+if platform.system() == "Windows":
+    PATH = 'C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe'
+    pytesseract.pytesseract.tesseract_cmd = PATH
+
+
+# for terminal Colors
+class Colors:  # pylint: disable=R0903, C0111
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
     OKGREEN = '\033[92m'
@@ -39,41 +44,78 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+
 # list of words to clean from the question during google search
-remove_words = []
+REMOVE_WORDS = []
 
 # negative words
-negative_words= []
+NEGATIVE_WORDS = []
 
-# load sample questions
+
 def load_json():
-    global remove_words, negative_words
-    path = "Data/settings.json"
+    """
+    Load the remove and negative words
+    that will be used for question parsing
+    """
 
-    settings = json.loads(open(path, encoding="utf8").read())
+    global REMOVE_WORDS, NEGATIVE_WORDS  # pylint: disable=W0603
+    settings_path = "Data/settings.json"
 
-    remove_words = settings["remove_words"]
-    negative_words = settings["negative_words"]
+    with open(settings_path, "r", encoding="utf8") as settings_file:
+        settings = json.loads(settings_file.read())
+
+        REMOVE_WORDS = settings["remove_words"]
+        NEGATIVE_WORDS = settings["negative_words"]
+
+        settings_file.close()
+
 
 # take screenshot of question
 def screen_grab(save, location):
-    im = ImageGrab.grab(bbox=(20, 260, 520, 700))
+    """Takes a screenshot of the user's screen
+
+    Arguments:
+        save {bool} -- Whether to save the image
+        location {string} -- The location to save the image if `save` is True
+
+    Returns:
+        Numpy Array -- The screenshotted image
+    """
+
+    img = ImageGrab.grab(bbox=(20, 260, 520, 700))
 
     if save:
-        im.save(location)
+        img.save(location)
 
-    return np.array(im)
+    return np.array(img)
+
 
 def preprocess_img(img):
+    """Preprocess image
+
+    Arguments:
+        img {Numpy Array} -- The image to be preprocessed
+
+    Returns:
+        Numpy Array -- The preprocessed image
+    """
+
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     gray = cv2.threshold(gray, 0, 255,
-            cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+                         cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
     gray = cv2.resize(gray, (0, 0), fx=0.5, fy=0.5)
     return gray
 
-# get OCR text //questions and options
+
 def read_screen():
+    """Takes a screenshot and processes the image.
+    Then feeds the image to Google Tesseract OCR
+
+    Returns:
+        String -- The string(s) found in the image
+    """
+
     spinner = Halo(text='Reading screen', spinner='bouncingBar')
     spinner.start()
 
@@ -81,17 +123,22 @@ def read_screen():
     image = screen_grab(save=False, location=screenshot_file)
     gray = preprocess_img(image)
 
-    # extract_text_time = time.time()
     # load the image as a PIL/Pillow image, apply OCR, and then delete the temporary file
     text = pytesseract.image_to_string(Image.fromarray(gray))
-    # print('\nExtract Text Elapsed Time: {}'.format(time.time() - extract_text_time))
 
     spinner.succeed()
     spinner.stop()
     return text
 
-# get questions and options from OCR text
+
 def parse_question():
+    """Gets the question and options from Tesseract
+
+    Returns:
+        String -- The retrieved question
+        List -- The options retrived from the screen
+    """
+
     text = read_screen()
     lines = text.splitlines()
     question = ""
@@ -100,51 +147,81 @@ def parse_question():
 
     for line in lines:
         if not flag:
-            question= question + " " + line
-
-        if '?' in line :
-            flag=True
+            question = question + " " + line
+        if '?' in line:
+            flag = True
             continue
-
-        if flag :
-            if line != '' :
+        if flag:
+            if line != '':
                 options.append(line)
 
     return question, options
 
-# simplify question and remove which,what....etc //question is string
+
 def simplify_ques(question, debug=False):
+    """Preprocesses the question for the Google Search
+    Removes all `REMOVE_WORDS` from the question
+
+    Arguments:
+        question {string} -- Original question
+
+    Keyword Arguments:
+        debug {bool} -- Whether to load json for debugging (default: {False})
+
+    Returns:
+        String -- The preprocessed question
+        Bool -- Whether the question contains a `NEGATIVE_WORD`
+    """
     if debug is True:
         load_json()
 
     neg = False
     qwords = question.lower().split()
     for i in qwords:
-        if i in negative_words:
+        if i in NEGATIVE_WORDS:
             neg = True
 
-    cleanwords = [word for word in qwords if word.lower() not in remove_words]
+    cleanwords = [word for word in qwords if word.lower() not in REMOVE_WORDS]
     temp = ' '.join(cleanwords)
 
-    clean_question=""
-    #remove ?
-    for ch in temp:
-        if ch != "?" or ch != "\"" or ch != "\'":
-            clean_question=clean_question+ch
+    clean_question = ""
+    for letter in temp:
+        if letter != "?" or letter != "\"" or letter != "\'":
+            clean_question = clean_question + letter
 
     return clean_question.lower(), neg
 
-# answer by combining two words
+
 def smart_answer(content, qwords):
-    zipped= zip(qwords,qwords[1:])
-    points=0
-    for el in zipped:
-        if content.count(el[0]+" "+el[1])!=0 :
-            points+=1000
+    """Answer by combining two words
+
+    Arguments:
+        content {bytes} -- The HTML of the webpage to search for `qwords`
+        qwords {list} -- List of string containg each word of the question
+
+    Returns:
+        int -- The points calculated
+    """
+
+    zipped = zip(qwords, qwords[1:])
+    points = 0
+    for element in zipped:
+        if content.count(element[0] + " " + element[1]) != 0:
+            points += 1000
+            print(points)
     return points
 
-# split the string
+
 def split_string(source):
+    """Remove
+
+    Arguments:
+        source {string} -- The string to be split
+
+    Returns:
+        string -- Split string
+    """
+
     splitlist = ",!-.;/?@ #"
     output = []
     atsplit = True
@@ -159,19 +236,45 @@ def split_string(source):
                 output[-1] = output[-1] + char
     return output
 
-# get web page
+
 def get_page(link):
+    """Get a webpage
+
+    Arguments:
+        link {string} -- The website to read the HTML from.
+
+    Returns:
+        Bytes -- The HTML retrieved from the link
+    """
+
     try:
         if link.find('mailto') != -1:
             return ''
-        req = urllib2.Request(link, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'})
+
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'}
+        req = urllib2.Request(link, headers=headers)
         html = urllib2.urlopen(req).read()
         return html
-    except (urllib2.URLError, urllib2.HTTPError, ValueError) as e:
+    except (urllib2.URLError, urllib2.HTTPError, ValueError) as error:
+        print("Get Page Error: {}".format(error))
         return ''
 
-# use google to get wiki page
+
 def google_wiki(sim_ques, options, neg):
+    """Googles the individual options' wikipedia page and
+    finds each occurence of each word in the question.
+    Then adds them up for that question's points.
+
+    Arguments:
+        sim_ques {string} -- The preprocessed question
+        options {array_like} -- A list of options
+        neg {bool} -- Whether the question contains a word from `NEGATIVE_WORDS`
+
+    Returns:
+        List -- Contains each option with their respective score
+        String -- The highest scoring option
+    """
+
     spinner = Halo(text='Googling and searching Wikipedia', spinner='dots2')
     spinner.start()
 
@@ -183,18 +286,18 @@ def google_wiki(sim_ques, options, neg):
 
     maxp = -sys.maxsize
     words = split_string(sim_ques)
-    for o in options:
+    for option in options:
 
-        o = o.lower()
-        original=o
-        o += ' wiki'
+        option = option.lower()
+        original = option
+        option += ' wiki'
 
         # get google search results for option + 'wiki'
-        search_wiki = google.search(o, num_pages)
+        search_wiki = google.search(option, num_pages)
 
         link = search_wiki[0].link
         content = get_page(link)
-        soup = BeautifulSoup(content,"lxml")
+        soup = BeautifulSoup(content, "lxml")
         page = soup.get_text().lower()
 
         temp = 0
@@ -210,13 +313,26 @@ def google_wiki(sim_ques, options, neg):
         points.append(temp)
         if temp > maxp:
             maxp = temp
-            maxo=original
+            maxo = original
 
     spinner.succeed()
     spinner.stop()
     return points, maxo
 
+
 def process_search(option, sim_ques, neg, points):
+    """The worker function for processing each option's search
+
+    Arguments:
+        option {string} -- The option to search Google for
+        sim_ques {string} -- The question that is being processed and searched with `option`
+        neg {bool} -- Whether the question contains a word from `NEGATIVE_WORDS`
+        points {list} -- The point list being shared with all workers
+
+    Returns:
+        List -- Contains all the options and the one with the highest score
+    """
+
     option = option.lower()
     original = option
     option += " wiki"
@@ -236,121 +352,74 @@ def process_search(option, sim_ques, neg, points):
 
     for word in words:
         temp = temp + page.count(word)
-        #print("TEMP: {}".format(temp))
 
     temp += smart_answer(page, words)
-    #print("Smart Answer TEMP: {}".format(temp))
 
     if neg:
         temp *= -1
-        #print("Negative TEMP: {}".format(temp))
 
     points.append(temp)
     if temp > maxp:
         maxp = temp
         maxo = original
 
-    #print("MAXO: {}".format(points))
     return [points, maxo]
 
+
 def google_wiki_faster(sim_ques, options, neg):
+    """Googles the individual options' wikipedia page and
+    finds each occurence of each word in the question.
+    Then adds them up for that question's points.
+
+    Arguments:
+        sim_ques {string} -- The preprocessed question
+        options {array_like} -- A list of options
+        neg {bool} -- Whether the question contains a word from `NEGATIVE_WORDS`
+
+    Returns:
+        List -- Contains each option with their respective score
+        String -- The highest scoring option
+    """
+
     spinner = Halo(text='Googling and searching Wikipedia', spinner='dots2')
     spinner.start()
 
-    num_pages = 1
     points = list()
-
-    content = ""
     maxo = ""
-
-    #words = split_string(sim_ques)
-    #options = [o.lower() + ' wiki' for o in options]
 
     pool = Pool(processes=3)
 
-    process = partial(process_search, sim_ques=sim_ques, neg=neg, points=points)
+    process = partial(process_search, sim_ques=sim_ques,
+                      neg=neg, points=points)
     points = pool.map(process, options)
 
     pool.close()
     pool.join()
 
     maxo = ""
-    maxVal = -sys.maxsize
+    max_val = -sys.maxsize
     for option in points:
         val = option[0][0]
-        if val > maxVal:
+        if val > max_val:
             maxo = option
-            maxVal = val
+            max_val = val
 
     spinner.succeed()
     spinner.stop()
     return points, maxo
 
-# return points for sample_questions
-def get_points_sample():
-    simq = ""
-    x = 0
-    for key in sample_questions:
-        x = x + 1
-        points = []
-        simq, neg = simplify_ques(key)
-        options = sample_questions[key]
-        simq = simq.lower()
-        maxo = ""
-        points, maxo = google_wiki(simq, options, neg)
-        print("\n" + str(x) + ". " + bcolors.UNDERLINE + key + bcolors.ENDC + "\n")
-        for point, option in zip(points, options):
-            if maxo == option.lower():
-                option=bcolors.OKGREEN + option + bcolors.ENDC
-            print(option + " { points: " + bcolors.BOLD + str(point) + bcolors.ENDC + " }\n")
 
 def get_points_live():
+    """Handles all the logic to screenshot a live game """
+
     start_time = time.time()
 
     neg = False
 
     parse_question_time = time.time()
     question, options = parse_question()
-    print('Parse Question Elapsed Time: {} seconds'.format(time.time() - parse_question_time))
-
-    simq = ""
-    points = []
-
-    #simplify_ques_time = time.time()
-    simq, neg = simplify_ques(question)
-    #print('Simplify Question Elapsed Time: {} seconds'.format(time.time() - simplify_ques_time))
-
-    maxo=""
-    m = 1
-    if neg:
-        m =- 1
-
-    google_start_time = time.time()
-    try:
-        points, maxo = google_wiki(simq, options, neg)
-    except IndexError as e:
-        print("Sorry nothing could be searched")
-
-    print("Points: {}".format(points))
-    print('Google Search Elapsed Time: {} seconds'.format(time.time() - google_start_time))
-
-    print("\n" + bcolors.UNDERLINE + question + bcolors.ENDC + "\n")
-    for point, option in zip(points, options):
-        if maxo == option.lower():
-            option = bcolors.OKGREEN+option+bcolors.ENDC
-        print(option + " { points: " + bcolors.BOLD + str(point * m) + bcolors.ENDC + " } \n")
-
-    print('Total Elapsed Time: {}'.format(time.time() - start_time))
-
-# return points for live game // by screenshot
-def get_points_live_v2():
-    start_time = time.time()
-
-    neg = False
-
-    parse_question_time = time.time()
-    question, options = parse_question()
-    print('Parse Question Elapsed Time: {} seconds'.format(time.time() - parse_question_time))
+    print('Parse Question Elapsed Time: {} seconds'.format(
+        time.time() - parse_question_time))
 
     simq = ""
     points = []
@@ -358,26 +427,71 @@ def get_points_live_v2():
     simq, neg = simplify_ques(question)
 
     maxo = ""
-    m = 1
+    modifier = 1
     if neg:
-        m =- 1
+        modifier = - 1
 
     google_start_time = time.time()
-    #points, maxo = google_wiki(simq, options, neg)
+    try:
+        points, maxo = google_wiki(simq, options, neg)
+    except IndexError as error:
+        print("Sorry nothing could be searched")
+        print("Error: {}".format(error))
+
+    print("Points: {}".format(points))
+    print('Google Search Elapsed Time: {} seconds'.format(
+        time.time() - google_start_time))
+
+    print("\n" + Colors.UNDERLINE + question + Colors.ENDC + "\n")
+    for point, option in zip(points, options):
+        if maxo == option.lower():
+            option = Colors.OKGREEN + option + Colors.ENDC
+        print(option + " { points: " + Colors.BOLD +
+              str(point * modifier) + Colors.ENDC + " } \n")
+
+    print('Total Elapsed Time: {}'.format(time.time() - start_time))
+
+
+def get_points_live_v2():
+    """ Handles all the logic to screenshot a live game
+    with faster a faster execution time then `get_points_live()`"""
+
+    start_time = time.time()
+
+    neg = False
+
+    parse_question_time = time.time()
+    question, options = parse_question()
+    print('Parse Question Elapsed Time: {} seconds'.format(
+        time.time() - parse_question_time))
+
+    simq = ""
+    points = []
+
+    simq, neg = simplify_ques(question)
+
+    maxo = ""
+    modifier = 1
+    if neg:
+        modifier = - 1
+
+    google_start_time = time.time()
     try:
         points, maxo = google_wiki_faster(simq, options, neg)
-    except IndexError as e:
+    except IndexError as error:
         print("Sorry nothing could be searched")
+        print("Error: {}".format(error))
+    print('Google Search Elapsed Time: {} seconds'.format(
+        time.time() - google_start_time))
 
-    print('Google Search Elapsed Time: {} seconds'.format(time.time() - google_start_time))
-
-    print("\n" + bcolors.UNDERLINE + question + bcolors.ENDC + "\n")
+    print("\n" + Colors.UNDERLINE + question + Colors.ENDC + "\n")
     for point, option in zip(points, options):
         if maxo[1] == option.lower():
-            option = bcolors.OKGREEN + option + bcolors.ENDC
+            option = Colors.OKGREEN + option + Colors.ENDC
         else:
-            option = bcolors.FAIL + option + bcolors.ENDC
-        print(option + " { points: " + bcolors.BOLD + str(point[0][0] * m) + bcolors.ENDC + " } \n")
+            option = Colors.FAIL + option + Colors.ENDC
+        print(option + " { points: " + Colors.BOLD +
+              str(point[0][0] * modifier) + Colors.ENDC + " } \n")
 
     print('Total Elapsed Time: {}'.format(time.time() - start_time))
 
@@ -385,16 +499,13 @@ def get_points_live_v2():
 if __name__ == "__main__":
     load_json()
 
-    while(1):
-        keypressed = input('Press s to screenshot live game or q to quit:\n')
-        if keypressed == 's':
-            #p.apply_async(get_points_live(), ())
+    while True:
+        KEYPRESSED = input('Press s to screenshot live game or q to quit:\n')
+        if KEYPRESSED == 's':
             get_points_live_v2()
-        elif keypressed == 'o':
+        elif KEYPRESSED == 'o':
             get_points_live()
-        elif keypressed == 'q':
+        elif KEYPRESSED == 'q':
             break
         else:
-            print(bcolors.FAIL + "\nUnknown input" + bcolors.ENDC)
-
-
+            print(Colors.FAIL + "\nUnknown input" + Colors.ENDC)
--- tests/image_test.py
+++ tests/image_test.py
@@ -1,23 +1,21 @@
 import os
 import sys
 import unittest
-from random import shuffle
 import numpy as np
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 import answer_bot
+
 
 class ImageTestCase(unittest.TestCase):
     """ Tests functions relating to the images """
+
     def setUp(self):
         self.screen = answer_bot.screen_grab(save=False, location=None)
         self.preprocessed = answer_bot.preprocess_img(self.screen)
 
     def test_type_img(self):
-        """
-        Test the type returned from `screen_grab()` and `preprocess_img()`
-        """
+        """ Test the type returned from `screen_grab()` and `preprocess_img()` """
 
         self.assertIsInstance(
             self.screen, np.ndarray)
@@ -28,23 +26,24 @@ class ImageTestCase(unittest.TestCase):
 
     def test_img_dimensions(self):
         """ Test the dimensions of the image """
-        def imageInBounds(img, maxWidth=500, maxHeight=500):
+        def image_in_bounds(img, max_width=500, max_height=500):
             width, height, _ = img.shape
-            if width > maxWidth or height > maxHeight:
+            if width > max_width or height > max_height:
                 return False
             else:
                 return True
 
-        self.assertTrue(imageInBounds(self.screen))
+        self.assertTrue(image_in_bounds(self.screen))
 
         width, height, _ = self.screen.shape
-        newWidth, newHeight = self.preprocessed.shape
-        check = newWidth == width/2 and newHeight == height/2
+        new_width, new_height = self.preprocessed.shape
+        check = new_width == width / 2 and new_height == height / 2
         self.assertTrue(check)
 
     def test_preprocess_img(self):
         """ Test if the images are the same or not """
         self.assertFalse(np.array_equal(self.screen, self.preprocessed))
+
 
 if __name__ == '__main__':
     unittest.main()
