f7abebcdac6febcd6dc25767bcd50c8b4d65af17
abrad1212 <abrad12122@gmail.com>
2018-05-31 00:40:16 -0500

    Fixed version string

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name="HQ Bot",
-      version="1.0.0dev",
+      version="1.0.0",
       description="A HQ Trivia Bot",
       license="MIT",
 
